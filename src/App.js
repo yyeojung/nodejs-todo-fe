c166982e2e2889c4ceb8c93adacf2d350a1b1fe9
yyeojung <hongshii0i0i@gmail.com>
2025-07-24 21:20:54 +0900

    todoList create, get 완료

--- src/App.js
+++ src/App.js
@@ -6,8 +6,39 @@ import TodoBoard from "./components/TodoBoard";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import { useState } from "react";
+import api from "./utils/api";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  const getTasks = async () => {
+    const response = await api.get("tasks");
+    setTodoList(response.data.data);
+  };
+
+  const addTask = async () => {
+    try {
+      const response = await api.post("/tasks", {
+        task: todoValue,
+        isComplete: false,
+      });
+      if (response.status === 200) {
+        console.log("성공");
+        setTodoValue("");
+        getTasks();
+      } else {
+        throw new Error("task can not be added");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTasks();
+  }, []);
   return (
     <Container>
       <Row className="add-item-row">
@@ -16,10 +47,14 @@ function App() {
             type="text"
             placeholder="할일을 입력하세요"
             className="input-box"
+            value={todoValue}
+            onChange={(e) => setTodoValue(e.target.value)}
           />
         </Col>
         <Col xs={12} sm={2}>
-          <button className="button-add">추가</button>
+          <button className="button-add" onClick={addTask}>
+            추가
+          </button>
         </Col>
       </Row>
 
