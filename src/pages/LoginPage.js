ac04b5dd4877c1b6bf30a843a6ef25e4a3f2402d
yyeojung <hongshii0i0i@gmail.com>
2025-07-26 21:57:33 +0900

    로그인 로직

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,22 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../utils/api";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isShowPassword, setIsShowPassword] = useState(false);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState("");
+  const [animateError, setAnimateError] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await api.post("/user/login", { email, password });
+
+      if (response.status === 200) {
+        setUser(response.data.user);
+        sessionStorage.setItem("token", response.data.token);
+        api.defaults.headers["authorization"] = "Bearer " + response.data.token;
+        setError("");
+        navigate("/");
+      } else {
+        throw new Error(response.data.error);
+      }
+    } catch (error) {
+      setError(error.message);
+      setAnimateError(true);
+      setTimeout(() => setAnimateError(false), 400);
+    }
+  };
   return (
-    <div className="display-center">
+    <div className="display-center" onSubmit={handleLogin}>
+      {error && (
+        <div className={animateError ? "error shake" : "error"}>{error}</div>
+      )}
       <Form className="login-box">
         <h1>로그인</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <div className="password-box">
+            <Form.Control
+              type={isShowPassword ? "text" : "password"}
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              className="show-password"
+              onClick={() => setIsShowPassword((prev) => !prev)}
+            >
+              {isShowPassword ? "🙈" : "🙉"}
+            </button>
+          </div>
         </Form.Group>
         <div className="button-box">
           <Button type="submit" className="button-primary">
