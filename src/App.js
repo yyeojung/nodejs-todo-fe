e7833abf0847c78eb5d9952696cf2b411cf300fe
yyeojung <hongshii0i0i@gmail.com>
2025-07-26 20:21:03 +0900

    App.js 리팩토링

--- src/App.js
+++ src/App.js
@@ -1,99 +1,18 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import TodoBoard from "./components/TodoBoard";
-
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import { useEffect, useState } from "react";
-import api from "./utils/api";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import TodoPage from "./pages/TodoPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [todoValue, setTodoValue] = useState("");
-
-  const getTasks = async () => {
-    const response = await api.get("tasks");
-    setTodoList(response.data.data);
-  };
-
-  const addTask = async () => {
-    try {
-      const response = await api.post("/tasks", {
-        task: todoValue,
-        isComplete: false,
-      });
-      if (response.status === 200) {
-        console.log("성공");
-        setTodoValue("");
-        getTasks();
-      } else {
-        throw new Error("task can not be added");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const updateTask = async (id, isComplete) => {
-    try {
-      const response = await api.put(`/tasks/${id}`, {
-        isComplete: !isComplete,
-      });
-      if (response.status === 200) {
-        console.log("수정성공");
-        getTasks();
-      } else {
-        throw new Error("task can not be added");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const deleteTask = async (id) => {
-    try {
-      const response = await api.delete(`/tasks/${id}`);
-      if (response.status === 200) {
-        console.log("삭제성공");
-        getTasks();
-      } else {
-        throw new Error("task can not be added");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    getTasks();
-  }, []);
   return (
-    <Container>
-      <Row className="add-item-row">
-        <Col xs={12} sm={10}>
-          <input
-            type="text"
-            placeholder="할일을 입력하세요"
-            className="input-box"
-            value={todoValue}
-            onChange={(e) => setTodoValue(e.target.value)}
-          />
-        </Col>
-        <Col xs={12} sm={2}>
-          <button className="button-add" onClick={addTask}>
-            추가
-          </button>
-        </Col>
-      </Row>
+    <Routes>
+      <Route path="/" element={<TodoPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
-      <TodoBoard
-        todoList={todoList}
-        onCompleteClick={updateTask}
-        onDeleteClick={deleteTask}
-      />
-    </Container>
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
