8679e64544b0773fca8620d90e17f67496ac3655
yyeojung <hongshii0i0i@gmail.com>
2025-07-26 23:09:49 +0900

    배포 page not found 오류 수정

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     <Routes>
-      <Route path="/todo" element={<TodoPage />} />
-      <Route path="/" element={<RegisterPage />} />
+      <Route path="/" element={<TodoPage />} />
+      <Route path="/register" element={<RegisterPage />} />
 
       <Route path="/login" element={<LoginPage />} />
     </Routes>
