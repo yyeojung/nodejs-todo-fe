40c1c6ccb268d324564dde805f4bb40b775aa266
yyeojung <hongshii0i0i@gmail.com>
2025-07-28 22:18:24 +0900

    author 추가 및 레이아웃 수정

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -7,19 +7,22 @@ const TodoItem = ({ item, deleteItem, toggleComplete }) => {
         <div className={`todo-item ${item.isComplete ? "item-complete" : ""}`}>
           <div className="todo-content">{item.task}</div>
 
-          <div>
-            <button
-              className="button-delete"
-              onClick={() => deleteItem(item._id)}
-            >
-              삭제
-            </button>
-            <button
-              className="button-delete"
-              onClick={() => toggleComplete(item._id)}
-            >
-              {item.isComplete ? `안끝남` : `끝남`}
-            </button>
+          <div className="todo-info">
+            <div className="author">{item.author.name}</div>
+            <div className="todo-btn">
+              <button
+                className="button-delete"
+                onClick={() => deleteItem(item._id)}
+              >
+                삭제
+              </button>
+              <button
+                className="button-delete"
+                onClick={() => toggleComplete(item._id)}
+              >
+                {item.isComplete ? `안끝남` : `끝남`}
+              </button>
+            </div>
           </div>
         </div>
       </Col>
