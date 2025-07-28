b1743c8d8e07e64a9e724e8e67c8d11fa642f558
yyeojung <hongshii0i0i@gmail.com>
2025-07-28 22:19:44 +0900

    user인 경우 todo "/"

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,14 +2,13 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-import { Link, useNavigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import api from "../utils/api";
 
-const LoginPage = () => {
+const LoginPage = ({ user, setUser }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isShowPassword, setIsShowPassword] = useState(false);
-  const [user, setUser] = useState(null);
   const [error, setError] = useState("");
   const [animateError, setAnimateError] = useState(false);
 
@@ -35,6 +34,10 @@ const LoginPage = () => {
       setTimeout(() => setAnimateError(false), 400);
     }
   };
+
+  if (user) {
+    return <Navigate to="/" />;
+  }
   return (
     <div className="display-center" onSubmit={handleLogin}>
       {error && (
